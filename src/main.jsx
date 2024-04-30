d82b5012c43ace05448f804a0e912b3b081c2de2
JaiThakur10 <dev10jai@gmail.com>
2024-04-30 16:06:11 +0530

    services

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Home from './components/Home.jsx'
+import Services from './components/Services.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Home/>
+    <Services/>
   </React.StrictMode>,
 )
